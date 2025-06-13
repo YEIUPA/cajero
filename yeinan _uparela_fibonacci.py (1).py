3ee6eea4c3d35824fa38e117973102b23d2a55fd
yeiupa <uparela.arango@gmail.com>
2025-06-13 17:33:30 -0500

    trabajos curso mujeres TIC

---
+++ yeinan _uparela_fibonacci.py (1).py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 21 18:15:44 2025
+
+@author: LENOVO
+"""
+
+i1= 0
+i2=1
+usuario=int( input("ingrese un nùmero"))
+for i in range(usuario):
+    resultado= (i1+i2)
+    print("el resultado de la secuencia",i+1,"es:",resultado)
+    i1=i2
+    i2=resultado
+          
+    
+'''
+FIBONACCI = 5
+TOTAL = 5
+'''
